68f81a0ecbb85d2dbac16b016d13864815f54fa6
LeszczynskiK <leszczynskikrzysiu@gmail.com>
2025-03-20 11:59:13 +0100

    added template foor queue of task which base on structure with 3 fields , in main implemented some test ones to check is is working

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include "iostream"
 using namespace std;
 
+#include "kolejka_szablon.hpp"//template of queue functionality
+
 int main()
 {
-
+    szablon_kolejki<int, string> queue1;//T1 is in type(time) , T2 is string type(name of task)
+    queue1.addTask(2, 3,"task 1");//priority, duration, name of task
+    queue1.addTask(1, 5,"taks 2");
+    queue1.addTask(3, 2,"task 3");
+    queue1.display();
 
 
     return 0;
